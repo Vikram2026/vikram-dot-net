39c03cb3de6943852f1b04a092731ef15609b83e
vikram patil <vikrampatil8483@gmail.com>
2022-05-24 13:08:50 -0700

    first commit

---
+++ PALINDROME/PALINDROME/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+
+        string s, revs = "";
+        Console.WriteLine(" Enter string");
+        s = Console.ReadLine();
+        for (int i = s.Length - 1; i >= 0; i--)  
+        {
+            revs += s[i].ToString();
+        }
+        if (revs == s) 
+        {
+            Console.WriteLine("String is Palindrome ");
+        }
+        else
+        {
+            Console.WriteLine("String is not Palindrome ");
+        }
+        Console.ReadKey();
+    }
+}
