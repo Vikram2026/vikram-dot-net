d5e72dafbde3965f8eb0fdc7563aa6a85caae801
vikram patil <vikrampatil8483@gmail.com>
2022-05-24 12:17:20 -0700

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("");
+        Console.WriteLine(" enter 1 for + 2 for  - 3 for * 4 for %");
+        int oper = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter 1st number");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter 2nd number");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        if (oper == 1)
+        {
+            Console.WriteLine("ans is " + (a + b));
+        }
+        if (oper == 2)
+        {
+            Console.WriteLine("ans is " + (a - b));
+        }
+        if (oper == 3)
+        {
+            Console.WriteLine("ans is " + (a * b));
+        }
+        if (oper == 4)
+        {
+            Console.WriteLine("ans is " + (a / b));
+        }
+
+    }
+}
